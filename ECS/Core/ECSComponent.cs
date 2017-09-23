64af5e8cf195694c9c04245fa581db50de9414dd
Simon van der Weele <vanderweelesimon@gmail.com>
2017-09-23 10:15:51 +0200

    testing git integration w/ vs code

--- ECS/Core/ECSComponent.cs
+++ ECS/Core/ECSComponent.cs
@@ -5,6 +5,7 @@ namespace ECS.Core
     public abstract class ECSComponent : IQuerable
     {
         private int m_queryID;
+        private float m_gitTest = 25;
         public int QueryID { get { return m_queryID; } set { m_queryID = value; } }
 
     }
